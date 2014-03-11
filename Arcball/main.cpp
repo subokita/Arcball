4bcab191510b14d73ccbd46957e90d5ff4de67c3
Saburo Okita <saburookita@Saburos-MacBook-Air.local>
2014-03-11 08:39:18 +0100

    Initial Commit

---
+++ Arcball/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Arcball
+//
+//  Created by Saburo Okita on 11/03/14.
+//  Copyright (c) 2014 Saburo Okita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
